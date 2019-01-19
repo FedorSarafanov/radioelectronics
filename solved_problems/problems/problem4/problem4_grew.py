f8f433f47c9e341e37d4173cd1c39b8e44928e57
Unknown <ponur0kirill@gmail.com>
2019-01-19 19:26:19 +0300

    + 4 задача

---
+++ solved_problems/problems/problem4/problem4_grew.py
@@ -0,0 +1,21 @@
+from pylab import *
+from matplotlib import rc
+rc('text', usetex=True)
+rc('text.latex', preamble=[r'\usepackage[russian]{babel}',
+                         r'\usepackage{amsmath}',
+                        r'\usepackage{amssymb}'])
+def U(t):
+	E=1
+	U0=0.5
+	tau=5
+	U=(E-U0)*heaviside(t,1)*exp(-t)-E*exp(-t+tau)*heaviside(t-tau,1)
+	return U
+t=linspace(0,10,100)
+plot(t,U(t))
+ylabel(r'$U_{\text{вых}}$',fontsize=16)
+xlabel(r'$t$',fontsize=16)
+grid(which='major', linestyle='-')
+grid(which='minor', linestyle=':')
+minorticks_on()
+show()
+savefig('problem4_grew.png')
